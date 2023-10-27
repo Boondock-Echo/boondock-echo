3847c69e536242d3c2f697bcb836ca94bcaac68e
Kaushlesh Chandel <120183704+boondockadmin@users.noreply.github.com>
2023-10-27 17:56:32 -0500

    Checkin base code

--- web-app/public/assets/vendor/hs-fullscreen/src/js/methods/add-class.js
+++
@@ -1,5 +0,0 @@
-export default function fullscreenAddClass(el, settings) {
-	el.classList.add(settings.toggleClassName.slice(1))
-	
-	document.querySelector(settings.mainContainerSelector).classList.add(settings.preventScrollClassName.slice(1))
-}
--- web-app/public/assets/vendor/hs-fullscreen/src/js/methods/remove-class.js
+++
@@ -1,5 +0,0 @@
-export default function fullscreenRemoveClass(el, settings) {
-	el.classList.remove(settings.toggleClassName.slice(1))
-	
-	document.querySelector(settings.mainContainerSelector).classList.remove(settings.preventScrollClassName.slice(1))
-}
